bd51d535dca742d1755613f1cc364f8f3e07e3e0
Otabek <curbsidenyc26@gmail.com>
2026-06-22 15:43:39 -0400

    Fix Supabase URL typo

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
 export const supabase = createClient(
-  "https://hjtkodlbgaxueomrswgw.supabase.co",
+  "https://hjtkodlbgaxueomsrwgw.supabase.co",
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhqdGtvZGxiZ2F4dWVvbXNyd2d3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODEwMTMzNDgsImV4cCI6MjA5NjU4OTM0OH0.-xznbMeIwfmVbhFEUynFMkLnPVrnjmD84XXftNXb-yM"
 );
